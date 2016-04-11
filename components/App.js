87d2b864be89fd8bde9dfe77d8ff972da256bf2f
jeffcarbine <jeffcarbine@gmail.com>
2016-04-11 13:19:21 -0600

    Added geolocation.

--- components/App.js
+++ components/App.js
@@ -1,12 +1,25 @@
 var React = require('react');
 
 var App = React.createClass({
+
+	setGeolocation(address){
+
+		navigator.geolocation.getCurrentPosition(function(position) {
+			console.log('Getting GPS coordinates...');
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+			console.log('Coordinates are ' + latitude + ', ' + longitude);
+		});
+
+	},
+
 	render(){
 
 		return (
 
 			<div>
 				<h1>Hello World</h1>
+				<button class="btn btn-default"	onClick={this.setGeolocation}>Set Location</button>
 			</div>
 
 		);
